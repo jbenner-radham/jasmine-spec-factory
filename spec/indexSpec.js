66cd4dac1b8cd73aa2cdac2ba1bc06ed7e5b2be6
James Benner <james.benner@gmail.com>
2016-08-27 23:58:23 -0400

    Update project structure.

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,7 @@
 describe('Jasmine Spec Factory', function () {
     'use strict';
 
-    const module = require('../');
+    const module = require('../lib');
 
     it('is a function', function () {
         expect(module).toEqual(jasmine.any(Function));
