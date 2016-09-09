028f4038343fc6305d38f28a6b471c5017dac73f
James Benner <james.benner@gmail.com>
2016-09-09 14:50:53 -0400

    Another fix...

--- spec/templatesSpec.js
+++ spec/templatesSpec.js
@@ -1,47 +1,47 @@
-describe('templates', function () {
+describe('Templates', function () {
     'use strict';
 
-    const templates = require('../lib/Templates');
+    const Templates = require('../lib/Templates');
 
     it('is a class', function () {
-        expect(templates).toEqual(jasmine.any(Function));
+        expect(Templates).toEqual(jasmine.any(Function));
     });
 
     describe('get', function () {
         it('is a method', function () {
-            expect(templates.get).toEqual(jasmine.any(Function));
+            expect(Templates.get).toEqual(jasmine.any(Function));
         });
 
         it('maps the `getTemplate` method to the `templates` array', function () {
-            spyOn(templates, 'getTemplate').and.callThrough();
-            templates.get();
-            expect(templates.getTemplate).toHaveBeenCalled();
+            spyOn(Templates, 'getTemplate').and.callThrough();
+            Templates.get();
+            expect(Templates.getTemplate).toHaveBeenCalled();
         });
 
         it('returns an array', function () {
-            expect(templates.get()).toEqual(jasmine.any(Array));
+            expect(Templates.get()).toEqual(jasmine.any(Array));
         });
     });
 
     describe('getPath', function () {
         it('is a method', function () {
-            expect(templates.getPath).toEqual(jasmine.any(Function));
+            expect(Templates.getPath).toEqual(jasmine.any(Function));
         });
 
         it('returns a string', function () {
-            expect(templates.getPath()).toEqual(jasmine.any(String));
+            expect(Templates.getPath()).toEqual(jasmine.any(String));
         });
     });
 
     describe('getTemplate', function () {
         it('is a method', function () {
-            expect(templates.getTemplate).toEqual(jasmine.any(Function));
+            expect(Templates.getTemplate).toEqual(jasmine.any(Function));
         });
 
         it('calls the `getPath` method', function () {
-            spyOn(templates, 'getPath').and.callThrough();
-            templates.get();
-            expect(templates.getPath).toHaveBeenCalled();
+            spyOn(Templates, 'getPath').and.callThrough();
+            Templates.get();
+            expect(Templates.getPath).toHaveBeenCalled();
         });
     });
 });
